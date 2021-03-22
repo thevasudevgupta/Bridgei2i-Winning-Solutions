4ba715f1d46c1211a7389dd109dc801639cb6317
tanay2001 <60615590+tanay2001@users.noreply.github.com>
2021-03-22 17:39:01 +0530

    fixed an error

--- XLM-R/infer.py
+++ XLM-R/infer.py
@@ -3,14 +3,15 @@ import argparse
 from transformers import XLMRobertaTokenizer, AutoConfig, XLMRobertaForSequenceClassification
 
 
-#either take input from uer or predefined
+#either take input from user or predefined
+
 def predict(text):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     #text = ['New top 100 sports companies ', 'New Top 100 gadgets of the year 2021', 'यहां Apple के 5G स्मार्टफ़ोन की कीमत कितनी हो सकती है ','Infinity wars is the most high tech Avengers make']
     inputs = tokenizer(text, truncation= True, return_tensors= 'pt', padding=True)
     inputs = inputs.to(device)
     model = model.to(device)
-    labels = torch.tensor([1]*len(text)).unsqueeze(0) 
+    labels = torch.tensor([1]).unsqueeze(0) 
 
     outputs = model(**inputs, labels=labels)
     layer = torch.nn.Softmax(dim=1)
@@ -26,6 +27,6 @@ if __name__=="__main__":
     parser.add_argument('--text',default='',required = True, type=str,help='article/tweet')
     args = parser.parse_args()
 
-    #call fucntion
+    #call fucntion ,currently only supports one prediction not batch wise
     print(predict(args.text))
     
